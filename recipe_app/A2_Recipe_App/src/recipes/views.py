ff1803c2e460fee8749b255d192b0263373229af
ccashley12 <ccashley12@gmail.com>
2025-05-14 13:59:59 -0400

    implemented user authentication and exercise 2.6 task

--- recipe_app/A2_Recipe_App/src/recipes/views.py
+++ recipe_app/A2_Recipe_App/src/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 # Create your views here.
@@ -8,13 +9,13 @@ def home(request):
     return render(request, "recipes/recipes_home.html")
 
 # CBV "RecipeList", protected
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
 
     template_name = "recipes/collection.html"
 
 # CBV "RecipeDetail", protected
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
 
     template_name = "recipes/detail.html"
