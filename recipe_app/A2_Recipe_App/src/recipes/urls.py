1e2ee89d4aed141c891f54d3c160dbae7eead677
ccashley12 <ccashley12@gmail.com>
2025-05-16 00:40:54 -0400

    task 2.7 implemented forms for search and add recipes

--- recipe_app/A2_Recipe_App/src/recipes/urls.py
+++ recipe_app/A2_Recipe_App/src/recipes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home, RecipeListView, RecipeDetailView
+from .views import home, RecipeListView, RecipeDetailView, search, add_recipe
 
-app_name = 'recipes'
+app_name = "recipes"
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('collection', RecipeListView.as_view(), name='list'),
-    path('collection/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path("", home, name="home"),
+    path("collection", RecipeListView.as_view(), name="list"),
+    path("collection/<pk>", RecipeDetailView.as_view(), name="detail"),
+    path("search", search, name="search"),
+    path("add_recipe", add_recipe, name="add_recipe"),
 ]
